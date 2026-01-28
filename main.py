0645757e853cbd87e54584dfdb209f12c125d3c0
codeHysteria28 <graphicglobal67@gmail.com>
2026-01-28 16:00:59 +0000

    added a Player class and also created a circle shape class

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -17,7 +20,14 @@ def main():
                 return
             
         screen.fill("black")
+        
+        #? Draw the player
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
+
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
