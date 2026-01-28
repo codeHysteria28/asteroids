ef95ebd85498e337c42423bc13e97b5d28e89eeb
codeHysteria28 <graphicglobal67@gmail.com>
2026-01-28 16:16:57 +0000

    added move method to Player class, also modified game loop

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     print(f"\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -19,15 +20,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+
         screen.fill("black")
-        
+
         #? Draw the player
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         player.draw(screen)
 
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
